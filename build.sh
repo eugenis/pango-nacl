6e9bfe24c7be6a92f7db14dc6cfea20fa34de9e6
Evgeniy Stepanov <eugeni.stepanov@gmail.com>
2011-03-30 23:09:12 +0400

    Improve build.sh.

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
-PATH=$HOME/root/nacl-sdk/bin:$PATH CC=nacl64-gcc CXX=nacl64-g++ LD=nacl64-g++ AR=nacl64-ar RANLIB=nacl64-ranlib PKG_CONFIG_LIBDIR=$HOME/root/nacl-sdk/nacl64/usr/lib/pkgconfig ./configure --host=nacl64 --prefix=$HOME/root/nacl-sdk/nacl64/usr --without-x --enable-static --disable-shared
-PATH=$HOME/root/nacl-sdk/bin:$PATH make
-PATH=$HOME/root/nacl-sdk/bin:$PATH make install
+ROOT=${NACL_TOOLCHAIN_ROOT:-$HOME/root/nacl-sdk}
+
+PATH=$ROOT/bin:$PATH CC=nacl64-gcc CXX=nacl64-g++ LD=nacl64-g++ AR=nacl64-ar \
+  RANLIB=nacl64-ranlib PKG_CONFIG_LIBDIR=$ROOT/nacl64/usr/lib/pkgconfig \
+  ./configure --host=nacl64 --prefix=$ROOT/nacl64/usr --without-x --enable-static --disable-shared
+PATH=$ROOT/bin:$PATH make
+PATH=$ROOT/bin:$PATH make install
