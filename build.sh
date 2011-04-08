3fd8ebcb2a93c8af48c4f90232c5451d3cc1d66a
Evgeniy Stepanov <eugenis@google.com>
2011-04-08 12:08:01 +0400

    Tweak build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ ROOT=${NACL_TOOLCHAIN_ROOT:-$HOME/root/nacl-sdk}
 
 PATH=$ROOT/bin:$PATH CC=nacl64-gcc CXX=nacl64-g++ LD=nacl64-g++ AR=nacl64-ar \
   RANLIB=nacl64-ranlib PKG_CONFIG_LIBDIR=$ROOT/nacl64/usr/lib/pkgconfig \
-  ./configure --host=nacl64 --prefix=$ROOT/nacl64/usr --without-x --enable-static --disable-shared
+  ./configure --host=nacl64 --prefix=$ROOT/nacl64/usr --without-x --with-included-modules --without-dynamic-modules
 PATH=$ROOT/bin:$PATH make
 PATH=$ROOT/bin:$PATH make install
