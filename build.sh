48fbdb20946298dc104cba7e18fc334d5aed3718
Evgeniy Stepanov <eugeni.stepanov@gmail.com>
2011-05-08 21:45:33 +0400

    Fix build-time paths compiled into the binaries.

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ ROOT=${NACL_TOOLCHAIN_ROOT:-$HOME/root/nacl-sdk}
 
 PATH=$ROOT/bin:$PATH CC=nacl64-gcc CXX=nacl64-g++ LD=nacl64-g++ AR=nacl64-ar \
   RANLIB=nacl64-ranlib PKG_CONFIG_LIBDIR=$ROOT/nacl64/usr/lib/pkgconfig \
-  ./configure --host=nacl64 --prefix=$ROOT/nacl64/usr --without-x --with-included-modules --without-dynamic-modules
-PATH=$ROOT/bin:$PATH make
-PATH=$ROOT/bin:$PATH make install
+  ./configure --host=nacl64 --prefix=/usr --without-x --with-included-modules --without-dynamic-modules
+PATH=$ROOT/bin:$PATH make -j5
+PATH=$ROOT/bin:$PATH make install -j5 prefix=$ROOT/nacl64/usr
