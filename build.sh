45968e02a38157f95eaba79788881564ad2ad15d
Evgeniy Stepanov <eugenis@google.com>
2011-09-10 22:40:19 +0400

    Update the arch prefix.

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 ROOT=${NACL_TOOLCHAIN_ROOT:-$HOME/root/nacl-sdk}
 
-PATH=$ROOT/bin:$PATH CC=nacl64-gcc CXX=nacl64-g++ LD=nacl64-g++ AR=nacl64-ar \
-  RANLIB=nacl64-ranlib PKG_CONFIG_LIBDIR=$ROOT/nacl64/usr/lib/pkgconfig \
-  ./configure --host=nacl64 --prefix=/usr --without-x --with-included-modules --without-dynamic-modules
+PATH=$ROOT/bin:$PATH CC=x86_64-nacl-gcc CXX=x86_64-nacl-g++ LD=x86_64-nacl-g++ AR=x86_64-nacl-ar \
+  RANLIB=x86_64-nacl-ranlib PKG_CONFIG_LIBDIR=$ROOT/x86_64-nacl/usr/lib/pkgconfig \
+  ./configure --host=x86_64-nacl --prefix=/usr --without-x --with-included-modules --without-dynamic-modules
 PATH=$ROOT/bin:$PATH make -j5
-PATH=$ROOT/bin:$PATH make install -j5 prefix=$ROOT/nacl64/usr
+PATH=$ROOT/bin:$PATH make install -j5 prefix=$ROOT/x86_64-nacl/usr
